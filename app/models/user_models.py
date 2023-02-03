a6cbe38083415917c4bf97d5d810ad71363788f9
jigmam <jigma>
2023-02-03 17:10:51 +0100

    user models and routes

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy import Boolean, Column, Integer, Sequence, String
 from sqlalchemy.orm import relationship
 from .matter_models import Matter
 from ..test.database import Base
@@ -6,7 +6,7 @@ from ..test.database import Base
 
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     user = Column(String, unique=True, index=True)
     password = Column(String, index=True)
 
