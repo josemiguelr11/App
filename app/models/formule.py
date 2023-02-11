a018b468c56bcd3b470290b82131905d945943a7
Jose Rodriguez <102249020+josemiguelr11@users.noreply.github.com>
2023-02-11 20:35:42 +0100

    cambios para formule

--- app/models/formule.py
+++ app/models/formule.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import Union
 from .matter import Matter
 
 class FormulaBase(BaseModel):
-    id_primary: Matter
-    id_secondary: Matter
+    id_primary: int
+    id_secondary: int
     required: float
+    class Config:
+        orm_mode = True
     
